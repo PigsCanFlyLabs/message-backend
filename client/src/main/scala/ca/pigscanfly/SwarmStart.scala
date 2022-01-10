00f8d2c8084bcc596d3191a2c6de8b20cb672bbd
Jashan Goyal <jashangoyal98@gmail.com>
2022-01-10 22:42:25 +0530

    refactor: refactoring application name in logger

--- client/src/main/scala/ca/pigscanfly/SwarmStart.scala
+++
@@ -1,9 +0,0 @@
-package ca.pigscanfly
-
-import akka.http.scaladsl.model.headers.Cookie
-
-object SwarmStart {
-
-  val cookieHeader: Cookie = Cookie("JSESSIONID", "B120DCEBC05C9F6CE3FBCA259356C17E")
-
-}
