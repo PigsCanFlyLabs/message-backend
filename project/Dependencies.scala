99deb48f7b0a2a6342614189f53c259ffa8b339c
Jashan Goyal <jashangoyal98@gmail.com>
2022-02-01 21:42:47 +0530

    set up akka stmp dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 trait Versions {
   val typeConfVersion = "1.4.1"
-  val AkkaVersion = "2.6.8"
+  val AkkaVersion = "2.6.18"
   val AkkaHttpVersion = "10.2.7"
   val akkaHttpCirceVersion = "1.26.0"
   val ScalaTestVersion = "3.3.0-SNAP3"
@@ -13,7 +13,7 @@ trait Versions {
   val sendGridVersion = "4.8.1"
   val macwireVersion = "2.5.2"
   val macwireAkkaVersion = "2.5.2"
-  val jansiVersion ="2.4.0"
+  val jansiVersion = "2.4.0"
   val pureConfigVersion = "0.17.1"
   val slickVersion = "3.3.2"
   val mySqlVersion = "8.0.27"
@@ -22,6 +22,7 @@ trait Versions {
   val slickCircePgVersion = "0.19.4"
   val flywayVersion = "3.2.1"
   val jwtCirceVersion = "7.1.2"
+  val akkaSmtpVersion = "0.2.1.1"
 }
 
 object Dependencies extends Versions {
@@ -32,17 +33,17 @@ object Dependencies extends Versions {
   val actor = "com.typesafe.akka" %% "akka-actor" % AkkaVersion
   val akkaStream = "com.typesafe.akka" %% "akka-stream" % AkkaVersion
   val akkaHttp = "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion
-  val akkaHttpCirce   = "de.heikoseeberger" %% "akka-http-circe" % akkaHttpCirceVersion
+  val akkaHttpCirce = "de.heikoseeberger" %% "akka-http-circe" % akkaHttpCirceVersion
   val circeCore = "io.circe" %% "circe-core" % circeVersion
   val circeParser = "io.circe" %% "circe-parser" % circeVersion
   val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
   val twilio = "com.twilio.sdk" % "twilio" % twilioVersion
-  val akkaSmtp = "com.github.ajozwik" %% "akka-smtp-server" % "0.2.1.1"
+  val akkaSmtp = "com.github.ajozwik" %% "akka-smtp" % akkaSmtpVersion
   val sendGrid = "com.sendgrid" % "sendgrid-java" % sendGridVersion
   val macwire = "com.softwaremill.macwire" %% "macros" % macwireVersion % "provided"
   val macwireAkka = "com.softwaremill.macwire" %% "macrosakka" % macwireAkkaVersion % "provided"
-  val jansi           = "org.fusesource.jansi" % "jansi" % jansiVersion
-  val pureConfig      = "com.github.pureconfig" %% "pureconfig" % pureConfigVersion
+  val jansi = "org.fusesource.jansi" % "jansi" % jansiVersion
+  val pureConfig = "com.github.pureconfig" %% "pureconfig" % pureConfigVersion
 
   //Testing
   val scalaMock = "org.scalamock" %% "scalamock" % "4.4.0"
@@ -53,14 +54,14 @@ object Dependencies extends Versions {
 
 
   //DB
-  val slick           = "com.typesafe.slick" %% "slick" % slickVersion
-  val mySql        = "mysql" % "mysql-connector-java" % mySqlVersion
-  val slickPg         = "com.github.tminglei" %% "slick-pg" % slickPgVersion
-  val hikaricp        = "com.typesafe.slick" %% "slick-hikaricp" % hikaricpVersion
-  val slickCirce =  "com.github.tminglei" %% "slick-pg_circe-json" % slickCircePgVersion
+  val slick = "com.typesafe.slick" %% "slick" % slickVersion
+  val mySql = "mysql" % "mysql-connector-java" % mySqlVersion
+  val slickPg = "com.github.tminglei" %% "slick-pg" % slickPgVersion
+  val hikaricp = "com.typesafe.slick" %% "slick-hikaricp" % hikaricpVersion
+  val slickCirce = "com.github.tminglei" %% "slick-pg_circe-json" % slickCircePgVersion
   val flyway = "org.flywaydb" % "flyway-core" % flywayVersion
-  val jwtCirce =  "com.github.jwt-scala" %% "jwt-circe" % jwtCirceVersion
-  val slickMySql="com.foerster-technologies" %% "slick-mysql" % "1.0.0"
+  val jwtCirce = "com.github.jwt-scala" %% "jwt-circe" % jwtCirceVersion
+  val slickMySql = "com.foerster-technologies" %% "slick-mysql" % "1.0.0"
   val commonDependencies = List(
     akkaHttpCirce,
     typeConf,
