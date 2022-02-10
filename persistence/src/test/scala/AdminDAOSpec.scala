e635f465b225b2a318f961a775f4754829799b17
Jashan Goyal <jashangoyal98@gmail.com>
2022-02-10 23:47:17 +0530

    added test cases for admin schema

--- persistence/src/test/scala/AdminDAOSpec.scala
+++ persistence/src/test/scala/AdminDAOSpec.scala
@@ -1,3 +1,4 @@
+import ca.pigscanfly.components.AdminLogin
 import ca.pigscanfly.dao.AdminDAO
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Seconds, Span}
@@ -13,12 +14,34 @@ class AdminDAOSpec extends AsyncWordSpecLike with ScalaFutures with Matchers wit
 
   val adminDao = new AdminDAO()
 
+  val adminLoginRequest = AdminLogin("email", "password", "role")
+
   "AccountsDAOSpec service" should {
-    "be able to createUserLoginDetails for user" in {
+
+    "be able to get resource permissions" in {
+      whenReady(adminDao.getResourcePermissions) { res =>
+        res shouldBe(Seq(), Seq())
+      }
+    }
+
+    "not be able to check if admin exists" in {
       whenReady(adminDao.checkIfAdminExists("email", "role")) { res =>
         res shouldBe 0
       }
     }
+
+    "be able to create admin login" in {
+      whenReady(adminDao.createAdminUser(adminLoginRequest)) { res =>
+        res shouldBe 1
+      }
+    }
+
+    "be able to validate admin login" in {
+      whenReady(adminDao.validateAdminLogin(adminLoginRequest)) { res =>
+        res shouldBe 1
+      }
+    }
+
   }
 
 }
